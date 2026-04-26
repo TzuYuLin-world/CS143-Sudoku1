ae8f7956c4d59e9d7d9dfde694dbb3dc8c560fa7
TzuYuLin-world <angelinalin0802@gmail.com>
2026-04-25 21:51:25 -0700

    Update SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -25,7 +25,12 @@ public class SudokuBoard {
       return checkData() && checkRow() && checkCol() && miniSquare();
    }
 
-   public boolean checkData() {}
+   public boolean checkData() {
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[r].length; c++) {}
+      }
+      
+   }
 
    public boolean validRows{
       return true;
